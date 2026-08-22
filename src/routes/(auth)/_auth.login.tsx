536b3f82bf1901033fb614cd6150f4602621573f
Jesus Reyes <luisdejesus200122@gmail.com>
2026-08-21 22:37:02 -0600

    feat: implementar página de inicio de sesión con el componente LoginCard y lógica de autenticación

--- src/routes/(auth)/_auth.login.tsx
+++ src/routes/(auth)/_auth.login.tsx
@@ -1,9 +1,23 @@
 import { createFileRoute } from '@tanstack/react-router'
 
+import { useLogin } from '#/presentation/hooks/auth/useLogin'
+import { LoginCard } from '#/presentation/views/auth/LoginCard'
+
 export const Route = createFileRoute('/(auth)/_auth/login')({
     component: LoginPage,
 })
 
 function LoginPage() {
-    return <div>Hello "/(auth)/_auth/login"!</div>
+    const { control, onSubmit, enviando, errorLogin, verPassword, alternarVerPassword } = useLogin()
+
+    return (
+        <LoginCard
+            control={control}
+            onSubmit={onSubmit}
+            enviando={enviando}
+            errorLogin={errorLogin}
+            verPassword={verPassword}
+            alternarVerPassword={alternarVerPassword}
+        />
+    )
 }
