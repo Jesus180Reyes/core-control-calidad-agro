e2d1967e03bd1f37a564f95a1a1a693374bf26ea
Jesus Reyes <luisdejesus200122@gmail.com>
2026-09-01 18:57:16 -0600

    refactor: mejorar la estructura del hook useRechazarLote y optimizar la invalidación de consultas

--- src/presentation/hooks/rechazar-lote/useRechazarLote.tsx
+++ src/presentation/hooks/rechazar-lote/useRechazarLote.tsx
@@ -3,16 +3,19 @@ import { toast } from "sonner"
 
 import { useExecuteMutation } from "../shared/useExecuteMutation"
 import type { RechazarLoteSchema } from "#/presentation/schema/rechazar-lote/rechazarLoteSchema"
+import { useNavigate } from "@tanstack/react-router"
 
 export const useRechazarLote = ({ loteId }: { loteId: number }) => {
-    const queryClient = useQueryClient()
+    const queryClient = useQueryClient();
+    const navigate = useNavigate();
 
     const mutation = useExecuteMutation<{ ok: boolean, msg: string }, RechazarLoteSchema>(`/lotes/${loteId}/rechazar`, {
         method: 'PATCH',
         onSuccess: (_data) => {
+            void queryClient.invalidateQueries({ queryKey: ['lotes'] });
+            void queryClient.invalidateQueries({ queryKey: ['pesajes', 'byLote'] });
             toast.success(_data.msg)
-            void queryClient.invalidateQueries({ queryKey: ['lotes'] })
-            void queryClient.invalidateQueries({ queryKey: ['pesajes', 'byLote'] })
+            navigate({ to: '/inspeccion-clientes' });
         },
         onError(error) {
             toast.error(error.message)
